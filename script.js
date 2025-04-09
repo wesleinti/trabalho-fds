e44e58e2b444fcc53244330b8868d97b87d6487a
wesleinti <145802914+wesleinti@users.noreply.github.com>
2025-04-09 12:46:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector("#botao");
+let para2 = document.querySelector("#para2");
+botao.addEventListener("click",msg);
+
+function msg(){
+    para2.innerHTML="Coisas boas estão por vir!"
+}
+
